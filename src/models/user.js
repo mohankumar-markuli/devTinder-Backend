6c4f8fbf608fbe0d52f5617a750c362369baaccb
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-04-02 10:32:17 +0530

    validator lib

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const user = new mongoose.Schema({
     "firstName": {
@@ -13,11 +14,21 @@ const user = new mongoose.Schema({
         required: true,
         unique: true,
         lowercase: true,
-        trim: true
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Email is not valid");
+            }
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error("Enter a Strong Password");
+            }
+        }
     },
     age: {
         type: Number,
@@ -33,7 +44,12 @@ const user = new mongoose.Schema({
     },
     phoroUrl: {
         type: String,
-        default: "https://imgs.search.brave.com/9NHdH-oVtf1aR7DtrO_L3frUwQuxfPK3PA8-8BWKi3U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMzkv/ODQ0Lzk5Ni9zbWFs/bC9wcm9maWxlLXBo/b3RvLWlsbHVzdHJh/dGlvbi1pc29sYXRl/ZC1vbi13aGl0ZS1i/YWNrZ3JvdW5kLW1h/bGUtcGVyc29uLWRl/ZmF1bHQtcHJvZmls/ZS1ncmF5LXBob3Rv/LXBpY3R1cmUtYXZh/dGFyLW1hbi1zaWxo/b3VldHRlLXBsYWNl/aG9sZGVyLXVzZXIt/c3ltYm9sLWZyZWUt/dmVjdG9yLmpwZw"
+        default: "https://imgs.search.brave.com/9NHdH-oVtf1aR7DtrO_L3frUwQuxfPK3PA8-8BWKi3U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMzkv/ODQ0Lzk5Ni9zbWFs/bC9wcm9maWxlLXBo/b3RvLWlsbHVzdHJh/dGlvbi1pc29sYXRl/ZC1vbi13aGl0ZS1i/YWNrZ3JvdW5kLW1h/bGUtcGVyc29uLWRl/ZmF1bHQtcHJvZmls/ZS1ncmF5LXBob3Rv/LXBpY3R1cmUtYXZh/dGFyLW1hbi1zaWxo/b3VldHRlLXBsYWNl/aG9sZGVyLXVzZXIt/c3ltYm9sLWZyZWUt/dmVjdG9yLmpwZw",
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Photo URL is not valid");
+            }
+        }
     },
     about: {
         type: String,
@@ -44,7 +60,7 @@ const user = new mongoose.Schema({
         max: 5
     }
 },
-{
+    {
         timestamps: true
     }
 );
